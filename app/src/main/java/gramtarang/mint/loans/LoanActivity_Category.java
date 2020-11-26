abef12f96ea82dced156784d7ce285b11d84682c
Jaitej <171801120014@cutmap.ac.in>
2020-11-26 11:42:17 +0530

    Validations Added.

--- app/src/main/java/gramtarang/mint/loans/LoanActivity_Category.java
+++ app/src/main/java/gramtarang/mint/loans/LoanActivity_Category.java
@@ -6,10 +6,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.model.Dash;
 
@@ -39,7 +41,25 @@ public class LoanActivity_Category extends AppCompatActivity {
             public void onClick(View view) {
                 selected_bank=bank.getSelectedItem().toString().trim();
                 selected_scheme= scheme.getSelectedItem().toString().trim();
-                if (selected_bank.matches("Select Bank")){
+                Log.d("SELECTION","BANK"+selected_bank);
+                Log.d("SELECTION","SCHEME"+selected_scheme);
+                if(selected_bank.equals("Select Bank")){
+                    Toast.makeText(LoanActivity_Category.this,"Select Bank",Toast.LENGTH_SHORT).show();
+                }
+                if(selected_scheme.equals("Select Scheme")){
+                    Toast.makeText(LoanActivity_Category.this,"Select Scheme",Toast.LENGTH_SHORT).show();
+                }
+                if(!selected_bank.equals("Select Bank") && !selected_scheme.equals("Select Scheme")){
+                  //  Toast.makeText(LoanActivity_Category.this,"Success",Toast.LENGTH_SHORT).show();
+                   SharedPreferences preferences = getSharedPreferences(mypreference, Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putString("LoanBank", selected_bank);
+                    editor.putString("LoanScheme", selected_scheme);
+                    editor.commit();
+                    Intent intent = new Intent(getApplicationContext(),LoanActivity_MainScreen.class);
+                    startActivity(intent);
+                }
+              /*  if (selected_bank.matches("Select Bank")){
                     //
                 }
                 if (selected_scheme.matches("Select Scheme")){
@@ -53,7 +73,7 @@ public class LoanActivity_Category extends AppCompatActivity {
                     editor.commit();
                     Intent intent = new Intent(getApplicationContext(),LoanActivity_MainScreen.class);
                     startActivity(intent);
-                }
+                }*/
             }
         });
 
